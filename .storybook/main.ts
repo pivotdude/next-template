5e1009e96eba15eb41dbea82ea2d5407143c4b0e
pivotdude <egor.corostin@gmail.com>
2023-08-21 19:14:27 +0400

    init tailwind in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,16 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {},
+    },
+  ],
   staticDirs: ['../public'],
   framework: {
     name: '@storybook/nextjs',
